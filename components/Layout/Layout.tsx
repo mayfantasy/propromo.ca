93027938c5d8ad787cf1a22916ac42f73d875158
hansknowyou <yuelun123321@hotmail.com>
2020-08-08 02:07:50 -0400

    Edited todo

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -64,26 +64,23 @@ const Layout = observer((props: IProps) => {
         {/* <pre>
           {JSON.stringify(
             {
-              NEXT________: '7',
+              NEXT________: '1',
               '1': 'Real Nav Items',
-              // '2': 'Product Detail Page',
+              
               '3': 'Real Filters',
-              // '4': 'Sort Products',
-              // '5': 'Customer Login & Register & My Account Page',
+
               '5':
                 'Upload all products to shopify (Upload upload-design product first to do testing)',
-              // '6': 'Upload designs',
-              // '6': 'select design templates',
-              '7': 'Cart page & Checkout',
+
               '8': 'Refund & Return Policy page',
               '9': 'About us page',
               '10': 'Terms & policy page',
               '11': 'Connect to CRM',
               '12': 'Product Images & Category Images',
               '13': 'Documentation for product tag machanism on product detail page',
-              '14': 'Set 1 default product collection using tags for each product, this is important for navigation (SEO) and static generation',
-              '15': 'Shipping address (use default but changable) & Billing Address (Use shipping address but changable)',
-              '16': 'Payment step using webUrl',
+              
+              '14': 'Setup shopify payment settings and proceed test orders.'
+              
               '17': 'The note field'
             },
             null,
