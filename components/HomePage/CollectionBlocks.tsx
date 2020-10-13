66e009824827e9a2ab1ab538150a65199da9149b
hansknowyou <yuelun123321@hotmail.com>
2020-10-13 15:58:26 -0400

    Updated collection & desktop slider images

--- components/HomePage/CollectionBlocks.tsx
+++ components/HomePage/CollectionBlocks.tsx
@@ -28,9 +28,9 @@ const CollectionBlocks = (props: IProps) => {
    */
   const collectionList: ICollectionCardInfo[] = [
     {
-      name: 'Acrylic Signs',
-      handle: 'acrylic-sign',
-      imgSrc: '/square-placeholder.jpg'
+      name: 'Acrylic Sign Products',
+      handle: IProductCollectionHandles.acrylic_sign,
+      imgSrc: '/collection-images/acrylic-sign-products.jpg'
     },
     // Up comming products
     // {
@@ -41,28 +41,43 @@ const CollectionBlocks = (props: IProps) => {
     {
       name: 'Banner Stands',
       handle: IProductCollectionHandles.banner_stands,
-      imgSrc: '/square-placeholder.jpg'
+      imgSrc: '/collection-images/banner-stands.jpg'
     },
-    {
-      name: 'Brochure and Newspaper Holders',
-      handle: IProductCollectionHandles.brochure_and_newspaper_holders,
-      imgSrc: '/square-placeholder.jpg'
-    },
+    // {
+    //   name: 'Brochure and Newspaper Holders',
+    //   handle: IProductCollectionHandles.brochure_and_newspaper_holders,
+    //   imgSrc: '/square-placeholder.jpg'
+    // },
     {
       name: 'Crowd Control Systems',
       handle: IProductCollectionHandles.crowd_control_systems,
-      imgSrc: '/square-placeholder.jpg'
+      imgSrc: '/collection-images/crowd-control-systems.jpg'
     },
-    {
-      name: 'Flags and Accessories',
-      handle: IProductCollectionHandles.flags,
-      imgSrc: '/square-placeholder.jpg'
+       {
+      name: 'Floor Sign Holders',
+      handle: IProductCollectionHandles.floor_sign_holders,
+      imgSrc: '/collection-images/floor-sign-holders.jpg'
     },
-    {
-      name: 'Prints',
-      handle: IProductCollectionHandles.prints,
-      imgSrc: '/square-placeholder.jpg'
-    }
+       {
+      name: 'Frame Sign Holders',
+      handle: IProductCollectionHandles.frame_sign_holders,
+      imgSrc: '/collection-images/frame-sign-holders.jpg'
+    },
+       {
+      name: 'Metal A Frames',
+      handle: IProductCollectionHandles.metal_a_frame_sign_holders,
+      imgSrc: '/collection-images/metal-aframes.jpg'
+    },
+    // {
+    //   name: 'Flags and Accessories',
+    //   handle: IProductCollectionHandles.flags,
+    //   imgSrc: '/square-placeholder.jpg'
+    // },
+    // {
+    //   name: 'Prints',
+    //   handle: IProductCollectionHandles.prints,
+    //   imgSrc: '/square-placeholder.jpg'
+    // }
   ]
 
   /**
